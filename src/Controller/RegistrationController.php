f4a48a92b81e30ed30bdf69cac3063fdab420c59
= <=>
2021-02-07 10:57:20 +0200

    simple loghin with jwt tocken was created

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use App\Security\LoginAuthenticator;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
+
+/**
+ * @Route("/api/petshop", name="petshop")
+ */
+
+class RegistrationController extends AbstractController
+{
+    /**
+     * @Route("/register/add", name="app_register")
+     */
+    public function register(Request $request, UserPasswordEncoderInterface $passwordEncoder, GuardAuthenticatorHandler $guardHandler, LoginAuthenticator $authenticator): Response
+    {
+
+        $user = new User();
+        $content = $request->getContent();
+        $user->setUsername($content->name);
+        $user->setEmail($content->email);
+        $user->setPassword($passwordEncoder->encodePassword($content->password));
+        $user->setRoles(['ROLE_ADMIN']);
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($user);
+        $entityManager->flush();
+            // do anything else you need here, like send an email
+
+            return $guardHandler->authenticateUserAndHandleSuccess(
+                $user,
+                $request,
+                $authenticator,
+                'main' // firewall name in security.yaml
+            );
+
+
+
+    }
+}
